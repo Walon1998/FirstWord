bd89fc448611c4a5f3e01658d093f2f3dc5343a0
WaloNeville <neville.walo@gmail.com>
2018-10-07 21:42:39 +0200

    Done both ways

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <memory.h>
-#include <stdlib.h>
 
 struct FirstWord {
     int length;
-    char* word;
+    char *word;
 };
 
 
@@ -16,7 +14,7 @@ struct FirstWord first_word(char input_string[]) {
         i++;
     } while (a != '\0' && a != ' ');
 
-    char *out = malloc(i* sizeof(char));
+    char *out = malloc(i * sizeof(char));
 
 
     for (int j = 0; j < i - 1; ++j) {
@@ -24,17 +22,41 @@ struct FirstWord first_word(char input_string[]) {
 
     }
 
-    struct FirstWord word = {i-1, out};
+    struct FirstWord word = {i - 1, out};
 
 
     return word;
 }
 
 
+int first_word2(char *input_string, char **output_word) {
+    char a;
+    int i = 0;
+    do {
+        a = input_string[i];
+        i++;
+    } while (a != '\0' && a != ' ');
+
+    char *out = malloc(i * sizeof(char));
+
+    for (int j = 0; j < i - 1; ++j) {
+        out[j] = input_string[j];
+
+    }
+    *output_word = out;
+    return i - 1;
+}
+
 int main() {
     char input[] = "lorem ipsum";
     struct FirstWord word = first_word(input);
-    printf("The first word of \"%s\" is \"%s\" and it's length is %d", input, word.word, word.length);
+    printf("The first word of \"%s\" is \"%s\" and it's length is %d \n", input, word.word, word.length);
+
+    char *output;
+    int length = first_word2(input, &output);
+    printf("The first word of \"%s\" is \"%s\" and it's length is %d", input, output, length);
+
+
 
 
 
